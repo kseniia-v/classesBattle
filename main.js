32f47275359d3d1c38c31270aa34542f3e07f140
kseniia-v <x27kadis@gmail.com>
2019-06-28 02:11:05 +0300

    ready, done

--- main.js
+++ main.js
@@ -5,73 +5,112 @@ const Heavy = require('./heavy');
 
 var stage = 1;
 var maxStage = 5;
+var shots = 0;
 
 var doctor = new Doctor('Dr Watson');
 var soldier = new Soldier('Sherlock Holmes');
 var heavy = new Heavy('James Moriarty');
 
-var shots = 0;
-
 var units = {
-  doctors: 1,
-  holmes: 5,
+  doctors: 2,
+  holmes: 7,
   moriarties: 2
 }
 
+function finalState(unit){
+  console.log(unit.getName() + ' feels at ' + unit.getHealth() + ' health-points. He achieved the ' + unit.getLevel() + ' level and earned ' + unit.getXp() + ' points of experience');
+}
+
 function shoot(){
-  var result = Math.round(Math.random()*10) - Math.round(Math.random()*10);
-  result = result > 0 ? result : 0;
+  var result = Math.floor(Math.random()*4);
   return result;
 }
 
+function affect(who, whom, unitsWho, unitsWhom, shots){
+  var effect = who.action(unitsWho, shots);
 
-function affect(who, whom, units, shots){
-  var effect = who.action(units, shots);
   if(who instanceof Doctor){
-    this.addHealth.call(whom,effect);
+    if(whom.addHealth(effect)){
+      console.log(whom.getName() + ' cured with : ' + effect + ' points');
+    }
   }else{
-    who.takeDamage.call(whom,effect);
+    console.log(whom.getName() + ' damaged with : ' + effect + ' points');
+    whom.takeDamage(effect);
   }
-  if(who.isAlive.call(whom)){
-    console.log(who.getName.call(whom) + ' is still ready to fight!');
+  if(who instanceof Doctor){
+    return true;
+  }
+  if(who.isAlive.call(whom, unitsWhom)){
+    if (effect > 0) {console.log(whom.getName() + ' a bit hurts but still is ready to fight!');}
+    return true;
   }else{
-    console.log('Battle is over for ' + who.getName.call(whom));
+    console.log('Battle is over for ' + whom.getName() + ' :(');
+    console.log(who.getName() + ' won!');
+    finalState(who);
+    return false;
   }
 }
 
-//affect(who,whom,who's quantity,shots for gun owners);
+
 
 var battle = function(d,s,h){
 
-  //console.log('Now we will see a grandiose battle between nobble ' + soldier.getName() + ' and insidious ' + heavy.getName() + '!');
-//  console.log('As far as it is a fantastic battle, there is some magic used: ' + heavy.getName() + ' multiplied himself up to ' + h + '.');
-//  console.log('But ' + soldier.getName() + ' is not a dupe and multiplied himself up to ' + s + '.');
-//  console.log('Fearless and the the only ' + d + ' ' + doctor.getName() + ' will do his job if needed.');
-//  console.log('Scoundrel ' + heavy.getName() + ' attacks first!');
+  console.log('Now we will see a grandiose battle between nobble ' + soldier.getName() + ' and insidious ' + heavy.getName() + '!');
+  console.log('As far as it is a fantastic battle, there is some magic used: ' + heavy.getName() + ' multiplied himself up to ' + h + '.');
+  console.log('But ' + soldier.getName() + ' is not a dupe and multiplied himself up to ' + s + '.');
+  console.log('Fearless and the only ' + d + ' ' + doctor.getName() + ' will do his job if needed.');
+  console.log('Scoundrel ' + heavy.getName() + ' attacks first!');
 
-  while(stage <= maxStage && (soldier.isAlive() && heavy.isAlive())){
-    console.log(stage);
+  while(stage <= maxStage && (soldier.isAlive(s) && heavy.isAlive(h))){
+
+    console.log('\n\n ***** ' + stage + ' *****');
 
     shots = shoot();
-    console.log(heavy.getName() + ' shoots at ' + soldier.getName() + ' with ' + shots + ' shots.');
-    affect(heavy,soldier,h,shots);
-
-    console.log(doctor.getName() + ' cures his friend ' + soldier.getName());
-    affect(heavy,soldier,h,shots);
+    console.log('action 1: ' + heavy.getName() + ' shoots at ' + soldier.getName() + ' with ' + shots + ' shots.');
+    if(!affect(heavy,soldier,h,s,shots)){ break; }
 
 
+    console.log('action 2: ' + doctor.getName() + ' cures his friend ' + soldier.getName());
+    affect(doctor,soldier,d,s);
 
+    console.log('action 3: ' + soldier.getName() + ' pays back to his enemy.');
+    if(!affect(soldier,heavy,s,h)){ break; }
 
+    console.log('action 4: And one more kick!');
+    if(!affect(soldier,heavy,s,h)){ break; }
 
+    console.log('action 5: And again!!!' + soldier.getName() + ' is in a rage!');
+    if(!affect(soldier,heavy,s,h)){ break; }
 
-/*  if (stage < maxStage){
-    console.log('So we are going to the next stage of battle as fighters are alive.');
-  }else {
-    console.log('All are too exhausted to continue so battle is finished with the following result: ');
-  }*/
+    console.log('action 6: ' + doctor.getName() + ' is too kind... He sweared to heal anyone, even ' + heavy.getName());
+    affect(doctor,heavy,d,s);
+
+    console.log('action 7: ' + heavy.getName() + ' doesn\'t cool down and shoots at ' + soldier.getName() + ' with ' + shots + ' shots.');
+    if(!affect(heavy,soldier,h,s,shots)){ break; }
+
+    console.log('action 8: ' + doctor.getName() + ' cures his friend ' + soldier.getName());
+    affect(doctor,soldier,d,s);
+
+    console.log('action 9: ' + soldier.getName() + ' strikes ' + heavy.getName());
+    if(!affect(soldier,heavy,s,h)){ break; }
+
+    console.log('action 10: ' + doctor.getName() + ' is too kind... He sweared to heal anyone, even ' + heavy.getName());
+    affect(doctor,heavy,d,s);
+
+    if (stage < maxStage){
+      if(soldier.isAlive(s) && heavy.isAlive(h)){
+        console.log('So we are going to the next stage of battle as fighters are alive.');
+      }
+    }else {
+      console.log('All are too exhausted to continue so battle is finished with the following results: ');
+      finalState(soldier);
+      finalState(heavy);
+    }
+    
     stage++;
   }
 };
 
-
 battle(units.doctors,units.holmes,units.moriarties);
+
+module.exports = battle;
--- unit.js
+++ unit.js
@@ -6,7 +6,6 @@ function Unit(name){
 
 Unit.prototype._level = 1;
 Unit.prototype._xp = 0;
-Unit.prototype._power = 15;
 
 Unit.prototype.getName = function() {
   return this._name;
@@ -16,17 +15,29 @@ Unit.prototype.getLevel = function() {
   return this._level;
 }
 
-Unit.prototype.isAlive = function() {
-  return this._health > 0;
+Unit.prototype.getHealth = function() {
+  return this._health;
+}
+
+Unit.prototype.getXp = function() {
+  return this._xp;
+}
+
+Unit.prototype.isAlive = function(a) {
+  return a * this._health > 0;
 }
 
 Unit.prototype.addHealth = function(a) {
-  return this._health = this._health + a < this.MAXHEALTH ? this._health + a : this.MAXHEALTH;
+  var cured = this._health < this.MAXHEALTH;
+  this._health = this._health + a < this.MAXHEALTH ? this._health + a : this.MAXHEALTH;
+  return cured;
 }
 
 Unit.prototype.takeDamage = function(a) {
-  this._health = this._health - a * (this._resistance ? this._resistance : 1);
-  this._earnExperience(500);
+  this._health = this._health - a * (this._resistance !== undefined ? this._resistance : 1);
+  if(a > 0){
+    this._earnExperience(500);
+  }
 }
 
 Unit.prototype._earnExperience = function(a) {
@@ -36,15 +47,16 @@ Unit.prototype._earnExperience = function(a) {
 
 Unit.prototype._levelUp = function() {
   if (this._xp % 1000 === 0){
-    this._level += this._level + 1;
+    this._level = this._level + 1;
     console.log(this._name + ' has improved his level up to ' + this._level);
   }
 }
 
 //I thought units - it is a quantity of actions at a time (hits), but it appeared that there is an extra quantity for heavy - shots
 //so I decided to use "unit" as a quantity of "objects" finally
+//that's why the same unit quantity I've added to define isAlive to track full 'army'
 Unit.prototype.action = function(units, shots){
-  var stdPower = this._power * units * (shots ? shots : 1);
+  var stdPower = this._power * units * (shots === undefined ? 1 : shots);
   var result = this._level > 1 ? (stdPower + stdPower * this._level * 0.01) : stdPower;
   this._earnExperience(250);
   return result;
